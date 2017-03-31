827e6541cda6d52961f5fb053067dd1214fb08f7
Daren Alfred <daren1997@hotmail.co.uk>
2017-03-31 20:32:18 +0100

    Add files via upload

---
+++ Chapter 1/1.2.py
@@ -0,0 +1,9 @@
+number1 = int(raw_input("Give the first number "))
+number2 = int(raw_input("Give the second number "))
+number3 = int(raw_input("Give the third number "))
+number4 = int(raw_input("Give the fourth number "))
+number5 = int(raw_input("Give the fifth number "))
+tot = number1 + number2 + number3 + number4 + number5
+average = tot / 5
+print tot
+print average
