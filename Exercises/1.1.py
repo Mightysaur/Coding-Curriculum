202f0c9f086f9eabdc47acd654ef117ecc1cbc18
Daren Alfred <daren1997@hotmail.co.uk>
2017-03-31 20:25:45 +0100

    Add files via upload

---
+++ Exercises/1.1.py
@@ -0,0 +1,8 @@
+address1 = raw_input("Give the first line of the address ")
+address2 = raw_input("give the second line of the address ")
+address3 = raw_input("give the third line of the address ")
+address4 = raw_input("give the fourth line of the address ")
+print address1
+print address2
+print address3
+print address4
